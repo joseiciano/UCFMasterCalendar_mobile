333888fa0e215a90b181d8efa558d1ad66f29a64
joseiciano <icianojose12345@aol.com>
2020-04-12 11:39:50 -0400

    everything bork

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import {NativeRouter, Switch, Route, BackButton} from 'react-router-native';
 import Home from './src/pages/Home';
 import Events from './src/pages/Events';
 import Clubs from './src/pages/Clubs';
-import BigBrain from './src/pages/BigBrain';
 
 export default class App extends Component {
   render() {
